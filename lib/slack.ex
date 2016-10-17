2e1d1a85923d74d47c3d0f51f635a10465a485d4
Luke Ledet <luke@revelry.co>
2016-10-17 15:52:34 -0500

    Clean up and comments

--- lib/slack.ex
+++ lib/slack.ex
@@ -1,11 +1,14 @@
 defmodule Slack do
   @moduledoc """
-  Functions for workig with the Github API
+  Functions for workig with the Slack API
   """
 
   @api_url "https://slack.com/api"
   @api_token System.get_env("SLACK_TOKEN")
 
+  @doc """
+  Add a reaction to a message designated by channel id and timestamp
+  """
   def add_reaction(%{name: name, channel_id: channel_id, timestamp: timestamp}) do
     request = URI.encode_query([
       token: @api_token,
@@ -14,7 +17,7 @@ defmodule Slack do
       timestamp: timestamp
     ])
 
-    response = HTTPotion.post("#{@api_url}/reactions.add", [
+    HTTPotion.post("#{@api_url}/reactions.add", [
       headers: ["Content-Type": "application/x-www-form-urlencoded"],
       body: request
     ])
--- web/controllers/tarpon_controller.ex
+++ web/controllers/tarpon_controller.ex
@@ -5,7 +5,11 @@ defmodule Slax.TarponController do
 
   def start(conn, %{"text" => text, "channel_id" => channel_id, "timestamp" => timestamp}) do
     if Regex.match?(~r/tarpon/i, text) do
-      Slack.add_reaction(%{name: "fish", channel_id: channel_id, timestamp: timestamp})
+      Slack.add_reaction(%{
+        name: "fish",
+        channel_id: channel_id,
+        timestamp: timestamp
+      })
     end
 
     send_resp(conn, 200, "")
