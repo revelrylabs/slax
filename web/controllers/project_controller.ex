09196f2f51259fa4f43425384880f098f2c8f3dc
Bryan Joseph <bryanjos@gmail.com>
2017-02-03 11:27:23 -0600

    Trim repo name

--- web/controllers/project_controller.ex
+++ web/controllers/project_controller.ex
@@ -5,7 +5,7 @@ defmodule Slax.ProjectController do
   plug Slax.Plugs.VerifyUser
 
   def start(conn, %{"text" => "new " <> repo}) do
-    formatted_response = Slax.Project.new_project(repo, conn.assigns.current_user.github_access_token)
+    formatted_response = Slax.Project.new_project(String.trim(repo), conn.assigns.current_user.github_access_token)
     |> Slax.Project.format_results
 
     json conn, %{
