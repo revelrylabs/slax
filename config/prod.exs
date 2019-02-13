e10c7a7dccc81e85a488a084e80dc10be50d6b6c
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-12 18:59:39 -0600

    Fix app name

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ config :slax, SlaxWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hunit, Slax.Repo,
+config :slax, Slax.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
