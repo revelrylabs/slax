9df65f99022a41f3f06191bd5ecd85e5e0d8c59e
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-13 13:36:19 -0600

    Trying to track down token differences

--- lib/slax_web/plugs/verify_token.ex
+++ lib/slax_web/plugs/verify_token.ex
@@ -1,6 +1,7 @@
 defmodule Slax.Plugs.VerifySlackToken do
   import Phoenix.Controller, only: [text: 2]
   import Plug.Conn, only: [halt: 1]
+  require Logger
 
   def init(app_var) do
     tokens = Application.get_env(:slax, Slax.Slack)[:tokens]
@@ -8,6 +9,8 @@ defmodule Slax.Plugs.VerifySlackToken do
   end
 
   def call(%Plug.Conn{params: %{"token" => token}} = conn, local_token) do
+    Logger.info("token: #{inspect(token)}, local_token: #{inspect(local_token)}")
+
     case token == local_token do
       true ->
         conn
