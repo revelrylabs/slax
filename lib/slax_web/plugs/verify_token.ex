7abac14a97cdb538714a1d0b36823738980e7d44
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-13 13:44:08 -0600

    Trying to track down token differences

--- lib/slax_web/plugs/verify_token.ex
+++ lib/slax_web/plugs/verify_token.ex
@@ -5,11 +5,13 @@ defmodule Slax.Plugs.VerifySlackToken do
 
   def init(app_var) do
     tokens = Application.get_env(:slax, Slax.Slack)[:tokens]
+    Logger.info("init: token: #{inspect(Keyword.get(tokens, app_var))}")
+
     Keyword.get(tokens, app_var)
   end
 
   def call(%Plug.Conn{params: %{"token" => token}} = conn, local_token) do
-    Logger.info("token: #{inspect(token)}, local_token: #{inspect(local_token)}")
+    Logger.info("call: token: #{inspect(token)}, local_token: #{inspect(local_token)}")
 
     case token == local_token do
       true ->
