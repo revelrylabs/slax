bc1447df647bd23462e0e2da102ea7f035f7e75c
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-19 22:24:31 -0600

    Refactor slax_controller. Take away test support files from coverage

--- lib/slax_web/controllers/slax_controller.ex
+++ lib/slax_web/controllers/slax_controller.ex
@@ -6,30 +6,21 @@ defmodule SlaxWeb.SlaxController do
   plug(Slax.Plugs.VerifyUser)
 
   def start(conn, %{"response_url" => response_url, "text" => command}) do
-    do_start(
-      conn,
-      :handle_command,
-      [
-        %Slax.Commands.Context{
-          user: Map.get(conn.assigns, :current_user)
-        },
-        OptionParser.split(command),
-        response_url
-      ]
-    )
-  end
+    context = %Slax.Commands.Context{
+      user: Map.get(conn.assigns, :current_user)
+    }
 
-  defp do_start(conn, func, args) do
-    Task.start_link(__MODULE__, func, args)
+    command_args = OptionParser.split(command)
+
+    Task.start_link(fn ->
+      response = Commander.run(context, command_args)
+
+      Integrations.slack().send_message(response_url, %{
+        response_type: "in_channel",
+        text: response
+      })
+    end)
+
     send_resp(conn, 201, "")
-  end
-
-  def handle_command(context = %Slax.Commands.Context{}, command, response_url) do
-    response = Commander.run(context, command)
-
-    Integrations.slack().send_message(response_url, %{
-      response_type: "in_channel",
-      text: response
-    })
   end
 end
