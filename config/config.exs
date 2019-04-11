28b3c8ff290e10134d8bd1614c0b80b1dfa6b7fd
CJ Horton <thecjhorton@gmail.com>
2019-04-11 18:44:09 -0500

    use coveralls-multi

--- config/config.exs
+++ config/config.exs
@@ -10,8 +10,6 @@ config :phoenix, :json_library, Jason
 # General application configuration
 config :slax, ecto_repos: [Slax.Repo]
 
-config :excoveralls, endpoint: System.get_env("COVERALLS_ENDPOINT")
-
 # Configures the endpoint
 config :slax, SlaxWeb.Endpoint,
   url: [host: "localhost"],
