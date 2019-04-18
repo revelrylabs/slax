72b4eb8b940ff05017a71327e4232118c9dee7ff
J Patrick <jpatricknola@gmail.com>
2019-04-18 18:02:23 -0500

    deleted temp blockerbot controllers and routes

--- lib/slax_web/router.ex
+++ lib/slax_web/router.ex
@@ -12,9 +12,5 @@ defmodule SlaxWeb.Router do
     post("/project", ProjectController, :start)
     post("/sprint", SprintController, :start)
     post("/slax", SlaxController, :start)
-    post("/blocker", BlockerController, :start)
-
-
-    get("/blockerbot", BlockerbotController, :start)
   end
 end
