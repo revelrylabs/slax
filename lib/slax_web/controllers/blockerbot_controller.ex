0a5d4810391cf01cbf9ae10dd4e4db2c5b6cd95a
J Patrick <jpatricknola@gmail.com>
2019-04-18 12:37:53 -0500

    cleaned up

--- lib/slax_web/controllers/blockerbot_controller.ex
+++ lib/slax_web/controllers/blockerbot_controller.ex
@@ -13,28 +13,4 @@ def start(conn, _params) do
   text(conn, "ok")
 end
 
-
-
-# def start(conn, %{"channel" => channel_name}) do
-#   case Projects.get_project_for_channel(channel_name) do
-#     nil ->
-#       text(conn, "A project could not be found for this channel.")
-
-#     project ->
-#       repo = List.first(project.repos)
-
-#       params = %{
-#         repo_name:
-#         org_name:
-#         channel_name:
-#         webhook:
-
-#       }
-
-# end
-
-# get webhook and other needed infor from db to make github request
-# get issues of project
-
-
 end
