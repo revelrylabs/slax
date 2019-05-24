83294ac01719aecc5ae5a92e748d37bae07ead5a
HoffsMH <hecker.matthew@gmail.com>
2019-05-24 10:27:01 -0500

    delete uneeded line

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,5 @@
 Mox.defmock(Slax.HttpMock, for: Slax.Http.Behaviour)
 Mox.defmock(Slax.Commands.GithubCommandsMock, for: Slax.Commands.GithubCommands.Behaviour)
-Slax.Command.GithubCommandMock
 ExUnit.configure(exclude: [skip: true])
 ExUnit.start()
 Application.ensure_all_started(:bypass)
