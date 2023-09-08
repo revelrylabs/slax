6a5267554ffddcb849f8661df6005af44c188763
BunnyDunker <eliott.frilet@revelry.co>
2023-09-08 16:55:32 -0500

    Add insert, update and lookup methods

--- lib/slax/projects/project_repo.ex
+++ lib/slax/projects/project_repo.ex
@@ -13,4 +13,12 @@ defmodule Slax.ProjectRepo do
 
     timestamps()
   end
+
+  def changeset(params \\ %{}) do
+    cast(%__MODULE__{}, params, [:org_name, :repo_name, :project_id])
+  end
+
+  def token_changeset(project_repo, token, expiration_date) do
+    change(project_repo, %{token: token, expiration_date: expiration_date})
+  end
 end
--- lib/slax/projects/project_repos.ex
+++ lib/slax/projects/project_repos.ex
@@ -1,7 +1,8 @@
 defmodule Slax.ProjectRepos do
   use Slax.Context
 
-  alias Slax.{ProjectRepo, ProjectChannel}
+  alias Slax.{ProjectRepo, ProjectChannel, Projects}
+  alias Ecto.Multi
 
   def get_blockerbot_repos() do
     ProjectRepo
@@ -12,6 +13,49 @@ defmodule Slax.ProjectRepos do
       org_name: pr.org_name,
       channel_name: pc.channel_name
     })
+    |> Repo.all()
+  end
+
+  def create(repo) do
+    repo
+    |> ProjectRepo.changeset()
+    |> Repo.insert()
+  end
+
+  def create_repo_with_project(%{
+        project_name: project_name,
+        org_name: org_name,
+        repo_name: repo_name
+      }) do
+    Multi.new()
+    |> Multi.run(:project, fn _, _ ->
+      Projects.get_or_create_by_name(project_name)
+    end)
+    |> Multi.insert(
+      :repo,
+      &ProjectRepo.changeset(%{
+        org_name: org_name,
+        repo_name: repo_name,
+        project_id: &1.project.id
+      })
+    )
+    |> Repo.transaction()
+  end
+
+  def add_token_to_repos(repo_ids, token, expiration_date) do
+    repo_ids
+    |> Enum.reduce(Multi.new(), fn id, multi ->
+      Multi.update(
+        multi,
+        :"project_repo_#{id}",
+        ProjectRepo.token_changeset(Repo.get(ProjectRepo, id), token, expiration_date)
+      )
+    end)
+    |> Repo.transaction()
+  end
+
+  def get_all() do
+    ProjectRepo
     |> Repo.all()
   end
 end
