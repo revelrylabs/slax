7bdffa831135c874745e55a4baf2c8f96a22fa9d
BunnyDunker <eliott.frilet@revelry.co>
2023-09-14 15:42:00 -0500

    Ensure ssl is started before migrating

--- priv/repo/migrations/20230908181020_add_token_and_date_to_repo.exs
+++ priv/repo/migrations/20230908181020_add_token_and_date_to_repo.exs
@@ -2,6 +2,7 @@ defmodule Slax.Repo.Migrations.AddTokenAndDateToRepo do
   use Ecto.Migration
 
   def change do
+    Application.ensure_started(:ssl)
     alter table(:project_repos) do
       add(:token, :string)
       add(:expiration_date, :date)
