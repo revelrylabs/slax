306afd10599b5fcaae044dfed0f2d480de740f55
bford002 <bford0002@gmail.com>
2023-12-06 21:52:27 -0600

    channel schema

---
+++ lib/slax/channels/channel.ex
@@ -0,0 +1,17 @@
+defmodule Slax.Channels.Channel do
+  use Slax.Schema
+
+  @type t :: %__MODULE__{}
+  schema "channels" do
+    field(:channel_id, :string)
+    field(:name, :string)
+    field(:disabled, :boolean, default: false)
+
+    timestamps()
+  end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:id, :name, :disabled])
+  end
+end
