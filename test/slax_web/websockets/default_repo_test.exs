7eac00d5d1682ce6d474b1d1144ef0e3b7aa3802
Samuel Aaron Katz Keer <sam.keer@revelry.co>
2024-05-31 15:16:29 -0500

    fixed up final test issues

--- test/slax_web/websockets/default_repo_test.exs
+++ test/slax_web/websockets/default_repo_test.exs
@@ -5,26 +5,48 @@ defmodule SlaxWeb.DefaultRepo.Test do
   alias SlaxWeb.DefaultRepo
 
   setup do
-    %{channel_id: channel_id} = insert(:channel, channel_id: "ABCDEFG")
+    channel_id = "CHANNEL_ID"
+
+    %{
+      default_project_repo: default_project_repo
+    } =
+      insert(
+        :channel,
+        channel_id: channel_id,
+        name: "CHANNEL_NAME",
+        default_project_repo: build(:project_repo)
+      )
+
     repo = insert(:project_repo)
 
     values = %{
-    "abcdefg" => %{
-      "repo_select" => %{"selected_option" => %{"value" => repo}},
-      "channels_select_action" => %{"selected_channel" => channel_id},
+      "channel_id" => %{
+        "repo_select" => %{"selected_option" => %{"value" => repo.id}},
+        "channels_select_action" => %{"selected_channel" => channel_id},
         "type" => "static_select"
       }
     }
 
-    [channel_id: channel_id, values: values]
+    [
+      channel_id: channel_id,
+      values: values,
+      default_project_repo: default_project_repo
+    ]
   end
 
   test "sets default repo", %{channel_id: channel_id, values: values} do
-    # WIP - channels list needs to be parseable
+    %{id: initial_default_project_repo_id} = Slax.Channels.maybe_get_default_repo(channel_id)
+
     expect(
       Slax.HttpMock,
       :get,
-      fn _, _, _ -> {:ok, %HTTPoison.Response{status_code: 201, body: ~s<{"ok": true, "channels": #{List.to_string([id: "ABCDEFG"])}}>}} end
+      fn _, _, _ ->
+        {:ok,
+         %HTTPoison.Response{
+           status_code: 201,
+           body: ~s<{"ok": true, "channels": [{"id": "CHANNEL_ID", "name": "CHANNEL_NAME"}]}>
+         }}
+      end
     )
 
     event = %{
@@ -40,7 +62,8 @@ defmodule SlaxWeb.DefaultRepo.Test do
 
     assert :ok == DefaultRepo.handle_payload(event)
 
-    %{channel_id: test} = Slax.Channels.maybe_get_default_repo(channel_id)
-    assert channel_id == test
+    %{id: new_default_project_repo_id} = Slax.Channels.maybe_get_default_repo(channel_id)
+
+    assert new_default_project_repo_id != initial_default_project_repo_id
   end
 end
